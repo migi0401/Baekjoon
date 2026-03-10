ccd1a63130e20afdfde1b39630dfa6b63a37185f
Lee Mingi <104820707+migi0401@users.noreply.github.com>
2026-03-10 18:00:24 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -1,25 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+
+
 int main() {
-    int t;
-    cin>>t;
+    int arr[12];
+    arr[1]=1;
+    arr[2]=2;
+    arr[3]=4;
 
-    int v[12];
+    for (int i = 4; i < 12; i++) { //점화식 구현
+        arr[i] = arr[i-1] + arr[i-2] + arr[i-3];
+    }
 
-    int n;
+    int T;
+    cin >> T;
 
-    v[1]=1;
-    v[2]=2;
-    v[3]=4;
+    while (T--) {
+        int n;
+        cin >> n;
 
-    while (t--) {
-        cin>>n;    //n입력받기
-
-        for (int i=4;i<=n;i++) {
-            v[i]=v[i-1]+v[i-2]+v[i-3];
-        }
-
-        cout<<v[n]<<"\n";
+        cout<<arr[n]<<"\n";
     }
 }
